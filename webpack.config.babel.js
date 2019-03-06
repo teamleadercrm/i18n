07df3ddebc7329c01fab662bd037f2b64530f115
Lowie Benoot <lowie@teamleader.be>
2019-03-06 15:48:09 +0100

    :wrench: only import the languages we need by using the ContextReplacementPlugin

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,9 @@
 import webpack from 'webpack';
 import path from 'path';
 import PeerDependenciesExternalsPlugin from 'peer-deps-externals-webpack-plugin';
+import supportedLocales from './src/supportedLocales';
+
+const languagesToImport = supportedLocales.map(locale => locale.split('-')[0]);
 
 const config = {
   entry: './src/index.js',
@@ -11,7 +14,10 @@ const config = {
   module: {
     rules: [{ test: /\.js$/, include: path.resolve(__dirname, 'src'), use: ['babel-loader'] }],
   },
-  plugins: [new PeerDependenciesExternalsPlugin()],
+  plugins: [
+    new PeerDependenciesExternalsPlugin(),
+    new webpack.ContextReplacementPlugin(/react-intl[/\\]locale-data$/, new RegExp(languagesToImport.join('|'))),
+  ],
 };
 
 module.exports = config;
