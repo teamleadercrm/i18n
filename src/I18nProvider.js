1e2b1adc873b242960943fe591a94fccc663eb00
Rathes Sachchithananthan <rathes@aheenam.com>
2019-09-27 20:59:17 +0200

    ✨ Initialize using props locale

--- src/I18nProvider.js
+++ src/I18nProvider.js
@@ -1,6 +1,12 @@
 import { PureComponent } from 'react';
 
 class I18nProvider extends PureComponent {
+  state = {
+    locale: this.props.locale,
+    messages: {},
+    isLoading: true,
+  };
+
   render() {
     return this.props.children;
   }
