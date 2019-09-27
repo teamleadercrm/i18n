b38013a4c92f180673fda661ab7c2c6b58ee3ea5
Rathes Sachchithananthan <rathes@aheenam.com>
2019-09-27 20:58:58 +0200

    ✅ Ensure initial state

--- src/__tests__/I18nProvider.spec.js
+++ src/__tests__/I18nProvider.spec.js
@@ -12,4 +12,16 @@ describe('I18nProvider', () => {
     expect(rendered.children().length).toBe(1);
     expect(rendered).toMatchSnapshot();
   });
+
+  it('intializes with locale passed as prop', () => {
+    const rendered = mount(
+      <I18nProvider locale="ta">
+        <div>Hello World</div>
+      </I18nProvider>,
+    );
+
+    expect(rendered.state().locale).toEqual('ta');
+    expect(rendered.state().messages).toEqual({});
+    expect(rendered.state().isLoading).toEqual(true);
+  });
 });
